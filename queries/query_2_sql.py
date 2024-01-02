ee6f79f26edcc9ca3736a24daf8ab2408b1b31e3
despoinavdl <61871415+despoinavdl@users.noreply.github.com>
2024-01-02 02:23:30 +0200

    Create query_2_sql.py
    Query 2 SQL implementation

---
+++ queries/query_2_sql.py
@@ -0,0 +1,46 @@
+from pyspark.sql import SparkSession
+from pyspark.sql.types import StructField, StructType, TimestampType, DoubleType, StringType, IntegerType
+from pyspark.sql.functions import year, month, count, dense_rank, col, to_date
+from pyspark.sql.window import Window
+import time, datetime
+
+start_time = time.time()
+
+spark = SparkSession \
+    .builder \
+    .appName("SQL query 2") \
+    .getOrCreate()
+
+df = spark.read.csv("hdfs://okeanos-master:54310/data/total_crime.csv" \
+                ,header=True)
+
+df.createOrReplaceTempView("crime_records")
+
+
+query_string = """
+SELECT
+    part_of_day,
+    COUNT(*) AS crime_count,
+    DENSE_RANK() OVER (ORDER BY COUNT(*) DESC) AS rank
+FROM (
+    SELECT
+        CASE
+            WHEN (`TIME OCC`) >= 500 AND (`TIME OCC`) < 1200 THEN 'morning'
+            WHEN (`TIME OCC`) >= 1200 AND (`TIME OCC`) < 1700 THEN 'afternoon'
+            WHEN (`TIME OCC`) >= 1700 AND (`TIME OCC`) < 2100 THEN 'evening'
+            ELSE 'night'
+        END AS part_of_day
+    FROM
+        crime_records
+    WHERE
+        `Premis Desc` = 'STREET'
+) tmp
+GROUP BY
+    part_of_day
+ORDER BY
+    rank;
+"""
+
+final_query = spark.sql(query_string)
+final_query.show()
+print('Total time for SQL: ',time.time() - start_time , 'sec')
