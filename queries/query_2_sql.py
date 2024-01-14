b03f4ff929db8f6c847bae1220622b0584750668
despoinavdl <61871415+despoinavdl@users.noreply.github.com>
2024-01-14 19:57:12 +0200

    Update query_2_sql.py

--- queries/query_2_sql.py
+++ queries/query_2_sql.py
@@ -1,8 +1,5 @@
 from pyspark.sql import SparkSession
-from pyspark.sql.types import StructField, StructType, TimestampType, DoubleType, StringType, IntegerType
-from pyspark.sql.functions import year, month, count, dense_rank, col, to_date
-from pyspark.sql.window import Window
-import time, datetime
+import time
 
 start_time = time.time()
 
